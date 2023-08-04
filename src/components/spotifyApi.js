2ea2780748ec6fa7721588d6d21d1159a5b8030e
unknown <jemibol106@gmail.com>
2023-08-04 03:41:28 -0700

    commit_message

--- src/components/spotifyApi.js
+++ src/components/spotifyApi.js
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 
 const authEndPoint = "https://accounts.spotify.com/authorize?";
-const redirectURl = "https://miiju106.github.io/miiju-music-player/";
+const redirectURl = "https://miiju106.github.io/miiju-music-player";
 const scopes = ["user-library-read", "playlist-read-private"];
 const clientId = "cc7bf2f534b34132ae1fd593ed85cd56";
 
